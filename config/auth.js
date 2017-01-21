15637ac949946765c701b310c99a12faf2572671
Hieu Nguyen <hnnguyen45@gmail.com>
2017-01-20 19:49:07 -0800

    removes node_modules & setups up heroku urls for social logins

--- config/auth.js
+++ config/auth.js
@@ -5,16 +5,16 @@ module.exports = {
     'facebookAuth' : {
         'clientID' : '321887081544096',
         'clientSecret' : '99ec08ec20a701bd70a471ecebf4edc8',
-        'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+        'callbackURL': (process.env.NODE_ENV=='production') ? 'https://still-reaches-30553.herokuapp.com/auth/facebook/callback' : 'http://localhost:8080/auth/facebook/callback'
     },
     'googleAuth' : {
         'clientID' : '649501450931-4i1vfbia0ghfvmehrsoee1bgbh6qipnn.apps.googleusercontent.com',
         'clientSecret' : 'Cj3wbAsR_qknCsv9y3ASHSfB',
-        'callbackURL': 'http://localhost:8080/auth/google/callback'
+        'callbackURL': (process.env.NODE_ENV=='production') ? 'https://still-reaches-30553.herokuapp.com/auth/google/callback' : 'http://localhost:8080/auth/google/callback'
     },
         'twitterAuth' : {
         'consumerKey'        : '7nTwpShyRuzYJOdtZbOWOjCWx',
         'consumerSecret'     : 'c6u5jMZHdHEdtkOyhhbtmEtzGZUVS5vhTh3oBgqGclqPjZ3rUB',
-        'callbackURL'        : 'http://localhost:8080/auth/twitter/callback'
+        'callbackURL': (process.env.NODE_ENV=='production') ? 'https://still-reaches-30553.herokuapp.com/auth/twitter/callback' : 'http://localhost:8080/auth/twitter/callback'
     }
 }
